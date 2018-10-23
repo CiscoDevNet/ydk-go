81f730860f656ef4a0effe17d36f2126f62e3997
Yan <ygorelik@cisco.com>
2018-10-23 03:45:31 -0700

    Fixing Travis test

--- dependencies_centos.sh
+++ dependencies_centos.sh
@@ -1,16 +1,42 @@
 #!/bin/sh
 
+function print_msg {
+    echo -e "${MSG_COLOR}*** $(date): dependencies_centos.sh | $@ ${NOCOLOR}"
+}
+
+function run_cmd {
+    print_msg "Running command: $@"
+    $@
+    local status=$?
+    if [ $status -ne 0 ]; then
+        MSG_COLOR=$RED
+        print_msg "Exiting '$@' with status=$status"
+        exit $status
+    fi
+    return $status
+}
+
+# Terminal colors
+RED="\033[0;31m"
+NOCOLOR="\033[0m"
+YELLOW='\033[1;33m'
+MSG_COLOR=$YELLOW
+
+print_msg "Installing CentOS dependencies"
 yum install -y epel-release
 yum install -y git libxslt-devel libssh-devel libstdc++-static python-devel cmake3 python-pip which wget make sudo > /dev/null
-yum install -y https://devhub.cisco.com/artifactory/rpm-ydk/0.7.3/libydk-0.7.3-1.x86_64.rpm > /dev/null
 
+print_msg "Installing YDK 0.7.3 core library"
+run_cmd yum install -y https://devhub.cisco.com/artifactory/rpm-ydk/0.7.3/libydk-0.7.3-1.x86_64.rpm > /dev/null
+
+print_msg "Installing C++ version 5"
 yum install centos-release-scl -y > /dev/null
 yum install devtoolset-4-gcc* -y > /dev/null
 
 ln -sf /opt/rh/devtoolset-4/root/usr/bin/gcc /usr/bin/gcc
 ln -sf /opt/rh/devtoolset-4/root/usr/bin/g++ /usr/bin/g++
 
-echo "Installing Golang version 1.9.2"
+print_msg "Installing Golang version 1.9.2"
 wget https://storage.googleapis.com/golang/go1.9.2.linux-amd64.tar.gz &> /dev/null
 tar -zxf  go1.9.2.linux-amd64.tar.gz -C /usr/local/
 ln -s /usr/local/go/bin/go /usr/bin/go
--- dependencies_trusty.sh
+++ dependencies_trusty.sh
@@ -1,18 +1,45 @@
 #!/bin/bash
 
+function print_msg {
+	echo -e "${MSG_COLOR}*** $(date): dependencies_trusty.sh | $@ ${NOCOLOR}"
+}
+
+function run_cmd {
+	print_msg "Running command: $@"
+	$@
+	local status=$?
+	if [ $status -ne 0 ]; then
+		MSG_COLOR=$RED
+		print_msg "Exiting '$@' with status=$status"
+		exit $status
+	fi
+	return $status
+}
+
+# Terminal colors
+RED="\033[0;31m"
+NOCOLOR="\033[0m"
+YELLOW='\033[1;33m'
+MSG_COLOR=$YELLOW
+
+print_msg "Installing Trusty OS dependencies"
 sudo apt-get update > /dev/null
 sudo apt-get install libssh-dev libtool gdebi-core python3-dev python-dev -y > /dev/null
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y > /dev/null
 sudo apt-get update > /dev/null
+
+print_msg "Installing C++ version 5"
 sudo apt-get install cmake gcc-5 g++-5 -y > /dev/null
 sudo ln -f -s /usr/bin/g++-5 /usr/bin/c++
 sudo ln -f -s /usr/bin/gcc-5 /usr/bin/cc
 
+print_msg "Installing YDK 0.7.3 C++ core library"
 git clone https://github.com/ciscodevnet/ydk-cpp.git -b 0.7.3
 mkdir ydk-cpp/core/ydk/build
 cd ydk-cpp/core/ydk/build
 cmake -DCMAKE_BUILD_TYPE=Release .. 
 sudo make install
 cd -
+
 sudo easy_install pip
 sudo pip install pybind11
