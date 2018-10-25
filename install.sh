9e34cbbec103fbc2b646f401942fc65d68f3371a
Yan <ygorelik@cisco.com>
2018-10-25 05:45:04 -0700

    Fixing Travis test for Trusty

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function print_msg {
-	echo -e "${MSG_COLOR}*** $(date): install.sh | $@ ${NOCOLOR}"
+    echo -e "${MSG_COLOR}*** $(date): install.sh | $@ ${NOCOLOR}"
 }
 
 # Terminal colors
@@ -10,17 +10,18 @@ NOCOLOR="\033[0m"
 YELLOW='\033[1;33m'
 MSG_COLOR=$YELLOW
 
-if [[ $(uname) == "Linux" ]] && [[ -z "${GOPATH// }" ]]; then
-    export GOPATH="$(pwd)/golang"
-    print_msg "Setting GOPATH to $GOPATH"
-fi
-
-if [[ $(uname) == "Darwin" ]] && [[ -z "${GOPATH// }" ]]; then
+if [[ $(uname) == "Darwin" ]]; then
     source /Users/travis/.gvm/scripts/gvm
     gvm use go1.9.2
     print_msg "GOROOT: $GOROOT"
     print_msg "GOPATH: $GOPATH"
     print_msg "GO version: $(go version)"
+else
+    export GOPATH="$(pwd)/golang"
+    export GOROOT=/usr/local/go
+    export PATH=$GOROOT/bin:$PATH
+    print_msg "Setting GOROOT to $GOROOT"
+    print_msg "Setting GOPATH to $GOPATH"
 fi
 
 print_msg "Installing YDK-Go core package"
